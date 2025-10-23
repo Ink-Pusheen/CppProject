8dbe4405935e6137b4582fe280d91523812c7768
Aiden S. <ASchagren@gmail.com>
2025-10-23 16:23:00 -0700

    Key_Check

--- Source/MyCppProject/Private/Key_Pickup.cpp
+++ Source/MyCppProject/Private/Key_Pickup.cpp
@@ -4,6 +4,8 @@
 #include "Maze_Character.h"
 #include "Key_Pickup.h"
 #include "Door_Object.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "Engine/Engine.h"
 
 AKey_Pickup::AKey_Pickup()
@@ -14,17 +16,33 @@ AKey_Pickup::AKey_Pickup()
 
 void AKey_Pickup::CheckActorType(AActor* OverlappedActor, AActor* otherActor)
 {
-
-	if (GEngine) //Testing
+	if (otherActor)
 	{
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("Hello from C++ Actor!"));
+		USpringArmComponent* springArm = otherActor->FindComponentByClass<USpringArmComponent>();
+		//ACameraActor* Cam = otherActor->FindComponentByClass<ACameraActor>();
+
+		if (GEngine) //Testing
+		{
+			if(!springArm) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("Has no player input"));
+
+			else  GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("Has player input"));
+			
+		}
+
+		//Check if the object has a springarm
+		if (springArm)
+		{
+			OpenDoor();
+		}
+
+		////Checks if the other actor is the maze character
+		//if (otherActor->IsA(AMaze_Character::StaticClass()))
+		//{
+		//	//OpenDoor();
+		//}
 	}
 
-	//Checks if the other actor is the maze character
-	if (otherActor->IsA(AMaze_Character::StaticClass()))
-	{
-		OpenDoor();
-	}
+	
 }
 
 void AKey_Pickup::OpenDoor()
