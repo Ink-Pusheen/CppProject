d29d0632da54877a08cdb829777457f3548e2e8c
Ink-Pusheen <ASchagren@gmail.com>
2025-11-06 19:27:21 -0700

    Pickup_Base

---
+++ Source/MyCppProject/Public/Pickup_Base.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "Pickup_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYCPPPROJECT_API APickup_Base : public ATriggerBox
+{
+	GENERATED_BODY()
+	
+public:
+
+	APickup_Base(); //On build
+
+	//Function for checking the overlapping actor
+	UFUNCTION()
+	void CheckActorType(class AActor* OverlappedActor, class AActor* otherActor);
+
+	//Function to derive custom functions onto
+	UFUNCTION()
+	void OverlapFunction();
+
+};
